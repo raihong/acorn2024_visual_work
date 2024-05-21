d08e7d273582e1a1e0510d2bfe45a5cfada56500
hongminki <hellohmk98@naver.com>
2024-05-21 10:11:06 +0900

    현재까지 수업했던 html javascript 예제 추가함

---
+++ javascript/test02.js
@@ -0,0 +1,15 @@
+// test02.js
+
+/*
+    html 요소에 일어나는 이벤트 종류
+
+    클릭하면 => click
+    마우스를 커서를 올리면 => mouseover
+    마우스 커서가 요소를 벗어나면 => mouseout
+    마우스를 클릭하면 => mousedown
+    마우스를 클릭후 때면 =>mouseup
+    포커스가 오면 => focus
+    포커스를 잃으면 => blur    ... 등등
+*/
+
+document.querySelector("#one").innerText = "p1 입니다"
